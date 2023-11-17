0b47b8747c813706e996e6f135a74a86f2938a09
nijesmik <bkybb@duck.com>
2023-11-17 11:33:36 +0900

    Feat(BE-Brand): Brand API 구현 (#17)
    - BrandRestController 구현
    - BrandService 구현
    - BrandDao 인터페이스 구현
    - BrandMapper.xml 쿼리문 작성

--- OneShot_SpringBoot/src/main/java/oneshot/controller/BrandRestController.java
+++ OneShot_SpringBoot/src/main/java/oneshot/controller/BrandRestController.java
@@ -1,15 +1,68 @@
 package oneshot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import oneshot.model.dto.Brand;
+import oneshot.model.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.annotations.Api;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/brand")
 @Api(tags = "브랜드 컨트롤러")
 public class BrandRestController {
+    @Autowired
+    private BrandService brandService;
+
+    @PostMapping("/create")
+    @ApiOperation(value = "브랜드 추가")
+    public ResponseEntity<Integer> createBrand(Brand brand) {
+        int result = brandService.createBrand(brand);
+        if (result == 0) {
+            return new ResponseEntity<Integer>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<Integer>(result, HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    @ApiOperation(value = "브랜드 조회")
+    public ResponseEntity<?> getBrands() {
+        List<Brand> brands = brandService.getBrands();
+        if (brands == null) {
+            return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<List<Brand>>(brands, HttpStatus.OK);
+    }
+
+    @PatchMapping("/update/{brandId}")
+    @ApiOperation(value = "브랜드 수정")
+    public ResponseEntity<?> updateBrand(Brand brand, @PathVariable int brandId) {
+        int result = brandService.updateBrand(brand, brandId);
+        if (result == 0) {
+            return new ResponseEntity<Integer>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<Integer>(result, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{brandId}")
+    @ApiOperation(value = "브랜드 삭제")
+    public ResponseEntity<?> deleteBrand(@PathVariable int brandId) {
+        int result = brandService.deleteBrand(brandId);
+        if (result == 0) {
+            return new ResponseEntity<Integer>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<Integer>(result, HttpStatus.OK);
+    }
 
 }
--- OneShot_SpringBoot/src/main/java/oneshot/model/service/BrandServiceImpl.java
+++ OneShot_SpringBoot/src/main/java/oneshot/model/service/BrandServiceImpl.java
@@ -1,14 +1,44 @@
 package oneshot.model.service;
 
+import java.util.List;
+import oneshot.model.dao.BrandDao;
+import oneshot.model.dto.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import oneshot.model.dao.BrandDao;
-
 @Service
 public class BrandServiceImpl implements BrandService {
-
     @Autowired
     private BrandDao brandDao;
 
+    @Override
+    public int createBrand(Brand brand) {
+        return brandDao.insert(brand);
+    }
+
+    @Override
+    public List<Brand> getBrands() {
+        return brandDao.selectAll();
+    }
+
+    @Override
+    public int updateBrand(Brand newBrand, int brandId) {
+        newBrand.setBrandId(brandId);
+        Brand oldBrand = brandDao.selectOne(brandId);
+        if (newBrand.getBrandName() == null) {
+            newBrand.setBrandName(oldBrand.getBrandName());
+        }
+        if (newBrand.getLogo() == null) {
+            newBrand.setLogo(oldBrand.getLogo());
+        }
+        if (newBrand.getBrandUrl() == null) {
+            newBrand.setBrandUrl(oldBrand.getBrandUrl());
+        }
+        return brandDao.update(newBrand);
+    }
+
+    @Override
+    public int deleteBrand(int brandId) {
+        return brandDao.delete(brandId);
+    }
 }
