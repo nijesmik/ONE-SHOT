2c7b98a482a87cb1887be60d41fd6959b9ef0432
nijesmik <bkybb@duck.com>
2023-11-17 16:37:52 +0900

    Feat(BE-User): User API 구현 (#17)
    - UserRestController 구현
    - UserService 구현
    - UserDao 인터페이스 구현
    - UserMapper.xml 쿼리문 작성

--- OneShot_SpringBoot/src/main/java/oneshot/model/service/UserServiceImpl.java
+++ OneShot_SpringBoot/src/main/java/oneshot/model/service/UserServiceImpl.java
@@ -1,11 +1,10 @@
 package oneshot.model.service;
 
+import oneshot.model.dao.UserDao;
+import oneshot.model.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import oneshot.model.dao.UserDao;
-import oneshot.model.dto.User;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,16 +13,29 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
 
     @Override
-    public User login(User user) {
-        User tmp = userDao.login(user.getEmail());
-        if (tmp != null && tmp.getPassword().equals(user.getPassword()))
-            return tmp;
+    public User login(User inputUser) {
+        User savedUser = userDao.selectByEmail(inputUser.getEmail());
+        if (savedUser != null && savedUser.ComparePassword(inputUser)) {
+            return savedUser;
+        }
         return null;
     }
 
     @Transactional
     @Override
     public int signup(User user) {
-        return userDao.signup(user);
+        return userDao.insert(user);
+    }
+
+    @Override
+    public int updateUser(User user, int userId) {
+        User originalUserData = userDao.selectByUserId(userId);
+        user.importData(userId, originalUserData);
+        return userDao.update(user);
+    }
+
+    @Override
+    public int deleteUser(int userId) {
+        return userDao.delete(userId);
     }
 }
