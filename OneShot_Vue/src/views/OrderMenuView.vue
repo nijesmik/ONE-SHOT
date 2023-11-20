77d7c668f7c0600f149af47c3a74fc417515738a
nijesmik <bkybb@duck.com>
2023-11-20 14:38:35 +0900

    Feat(FE): 메뉴 선택시 공유 URL 추가 (#23)
    - 잘못된 orderCode로 접속시 Home화면으로 라우팅

--- OneShot_Vue/src/views/OrderMenuView.vue
+++ OneShot_Vue/src/views/OrderMenuView.vue
@@ -1,4 +1,9 @@
 <template>
+	<div class="input-group mb-3 url">
+  <input type="text" class="form-control" :placeholder="share_url" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+</div>
+
 	<div class="list">
 		<div v-for="menu in menus" class="menu select" @click="selectMenu(menu)">
 			<Menu :menu="menu" />
@@ -8,29 +13,41 @@
 <!-- --------------------------------------------------------------- -->
 <script setup>
 import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { useUrlStore } from "@/stores/url";
 import axios from "axios";
 import Menu from "@/components/Menu.vue";
 
+const URL = useUrlStore();
 const route = useRoute();
-const ORDER_API = `http://localhost:8080/api/order?orderCode=${route.params.orderCode}`;
-const MENU_API = "http://localhost:8080/api/menu?brandId=";
+const router = useRouter();
+const orderCode = route.params.orderCode;
 
+const share_url = ref(`${URL.DOMAIN}/order/${orderCode}`);
 const order = ref({});
 const orderDetails = ref([]);
 const menus = ref([]);
 
-axios.get(ORDER_API).then((res) => {
+axios.get(`${URL.API.ORDER}?orderCode=${orderCode}`).then((res) => {
 	order.value = res.data.order;
 	orderDetails.value = res.data.orderDetail;
-	axios.get(`${MENU_API}${order.value.brandId}`).then((res) => {
+	axios.get(`${URL.API.MENU}?brandId=${order.value.brandId}`).then((res) => {
 		menus.value = res.data;
 	});
-});
+}).catch((err) => {
+	if (err.response.status === 500) {
+		alert("잘못된 URL입니다.");
+		router.push({ name: "home" });
+	}
+})
 
 const selectMenu = (menu) => {
 	alert("TODO : 사이즈, ice/hot 선택 모달 띄우기");
 };
 </script>
 <!-- --------------------------------------------------------------- -->
-<style scoped></style>
+<style scoped>
+.url {
+	width: 90%;
+}
+</style>
