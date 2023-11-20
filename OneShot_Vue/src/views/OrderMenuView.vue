aa9c3ee9f9513a16aa66765e69373e6c1fe52562
nijesmik <bkybb@duck.com>
2023-11-20 17:40:02 +0900

    Feat(FE): 메뉴 타입 선택 (#32)

--- OneShot_Vue/src/views/OrderMenuView.vue
+++ OneShot_Vue/src/views/OrderMenuView.vue
@@ -1,14 +1,29 @@
 <template>
-	<ShareUrl :url="share_url"/>
+	<ShareUrl :url="share_url" />
+
+	<div class="type mb-3">
+		<button
+			class="btn btn-outline-info btn-sm"
+			v-for="type in menuTypes"
+			@click.prevent="selectType(type)"
+		>
+			{{ type }}
+		</button>
+	</div>
+
 	<div class="list">
-		<div v-for="menu in menus" class="menu select mb-3" @click="selectMenu(menu)">
+		<div
+			v-for="menu in computedMenus"
+			class="menu select mb-3"
+			@click="selectMenu(menu)"
+		>
 			<Menu :menu="menu" />
 		</div>
 	</div>
 </template>
 <!-- --------------------------------------------------------------- -->
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useUrlStore } from "@/stores/url";
 import axios from "axios";
@@ -23,7 +38,16 @@ const orderCode = route.params.orderCode;
 const share_url = ref(`${URL.DOMAIN}/order/${orderCode}`);
 const order = ref({});
 const orderDetails = ref([]);
+const menuTypes = ref([]);
 const menus = ref([]);
+
+const typeSelected = ref("");
+const selectType = (type) => {
+	typeSelected.value = type;
+};
+const computedMenus = computed(() => {
+	return menus.value.filter((menu) => menu.type === typeSelected.value);
+});
 
 axios
 	.get(`${URL.API.ORDER}?orderCode=${orderCode}`)
@@ -31,7 +55,9 @@ axios
 		order.value = res.data.order;
 		orderDetails.value = res.data.orderDetail;
 		axios.get(`${URL.API.MENU}?brandId=${order.value.brandId}`).then((res) => {
-			menus.value = res.data;
+			menuTypes.value = res.data.menuType;
+			menus.value = res.data.menuList;
+			typeSelected.value = menuTypes.value[0];
 		});
 	})
 	.catch((err) => {
@@ -47,7 +73,8 @@ const selectMenu = (menu) => {
 </script>
 <!-- --------------------------------------------------------------- -->
 <style scoped>
-.url {
-	width: 90%;
+.type {
+	display: flex;
+	flex-wrap: wrap;
 }
 </style>
