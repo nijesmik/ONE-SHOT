87bbc7c43f2501392946f605d0c5e2810e906653
nijesmik <bkybb@duck.com>
2023-11-20 16:51:38 +0900

    Fix(FE): 공유 url 컴포넌트로 분리 (#32)

---
+++ OneShot_Vue/src/components/ShareUrl.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="input-group mb-3 url">
+		<input
+			type="text"
+			class="form-control"
+			:placeholder="url"
+			aria-label="Recipient's username"
+			aria-describedby="button-addon2"
+		/>
+		<button class="btn btn-outline-secondary" type="button" id="button-addon2">
+			Copy
+		</button>
+	</div>
+</template>
+<!-- --------------------------------------------------------------- -->
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+	url: String,
+});
+</script>
+<!-- --------------------------------------------------------------- -->
+<style scoped></style>
