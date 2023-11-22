e865b1f0c22648fafe80a4a208dd3ddb6c6d6188
nijesmik <bkybb@duck.com>
2023-11-22 15:59:00 +0900

    Feat(FE): 홈 화면 이미지 추가 (#46)

--- OneShot_Vue/src/views/HomeView.vue
+++ OneShot_Vue/src/views/HomeView.vue
@@ -1,21 +1,19 @@
 <template>
-
-<router-link :to="{name : 'login'}" class="btn btn-outline-primary btn-lg">로그인</router-link>
-<router-link :to="{name : 'signup'}" class="btn btn-outline-primary btn-lg">회원가입</router-link>
-<router-link :to="{ name: 'order-brand' }" class="btn btn-outline-secondary btn-lg">비회원 주문</router-link>
-
+	<v-img src="/src/assets/coffee.jpg"></v-img>
+	<router-link :to="{ name: 'login' }" class="btn btn-outline-primary btn-lg"
+		>로그인</router-link
+	>
+	<router-link :to="{ name: 'signup' }" class="btn btn-outline-primary btn-lg"
+		>회원가입</router-link
+	>
+	<router-link
+		:to="{ name: 'order-brand' }"
+		class="btn btn-outline-secondary btn-lg"
+		>비회원 주문</router-link
+	>
+	<v-btn size="x-large" variant="plain" class="w-15">시작하기</v-btn>
 </template>
 <!-- --------------------------------------------------------------- -->
-<script setup>
-
-
-
-</script>
+<script setup></script>
 <!-- --------------------------------------------------------------- -->
-<style scoped>
-* {
-  margin: 1%;
-}
-
-
-</style>
+<style scoped></style>
