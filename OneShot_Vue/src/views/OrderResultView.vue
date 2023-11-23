57fecfc6c7e8a7751febdede27ea5da5d52ac17a
nijesmik <bkybb@duck.com>
2023-11-23 23:44:48 +0900

    Feat(FE): 결과 화면에 공유 URL 추가 (#47)
    - 공유 URL 스타일 수정
    - bootstrap 삭제

--- OneShot_Vue/src/views/OrderResultView.vue
+++ OneShot_Vue/src/views/OrderResultView.vue
@@ -1,6 +1,9 @@
 <template>
 	<v-container>
-		<v-card max-width="600" class="mx-auto p-3 mt-3">
+		<div class="url">
+			<ShareUrl />
+		</div>
+		<v-card max-width="600" class="mx-auto list">
 			<span class="title">주문 내역</span>
 			<v-list lines="two" v-for="orderDetail in menuStore.orderDetails">
 				<Result :order-detail="orderDetail" />
@@ -12,18 +15,23 @@
 <script setup>
 import { useMenuStore } from "@/stores/menu";
 import Result from "@/components/Result.vue";
+import ShareUrl from "@/components/ShareUrl.vue";
 
 const menuStore = useMenuStore();
 </script>
 <!-- --------------------------------------------------------------- -->
 <style scoped>
 .list {
-	width: auto;
-	margin: 0 auto;
+	padding: 2%;
 }
 
 .title {
 	font-size: 2.5rem;
 	font-weight: 500;
+}
+
+.url {
+	max-width: 600px;
+	margin: 0 auto;
 }
 </style>
