20854f94d5444a641b6ef62bc776241535842f8a
nijesmik <bkybb@duck.com>
2023-11-23 16:19:30 +0900

    Fix(FE): 로그인 상태에 따라 보여지는 메뉴가 달라지도록 수정 (#46)

--- OneShot_Vue/src/views/HomeView.vue
+++ OneShot_Vue/src/views/HomeView.vue
@@ -1,19 +1,66 @@
 <template>
-	<v-img src="/src/assets/coffee.jpg"></v-img>
-	<router-link :to="{ name: 'login' }" class="btn btn-outline-primary btn-lg"
-		>로그인</router-link
-	>
-	<router-link :to="{ name: 'signup' }" class="btn btn-outline-primary btn-lg"
-		>회원가입</router-link
-	>
-	<router-link
-		:to="{ name: 'order-brand' }"
-		class="btn btn-outline-secondary btn-lg"
-		>비회원 주문</router-link
-	>
-	<v-btn size="x-large" variant="plain" class="w-15">시작하기</v-btn>
+	<div class="background">
+		<div class="title">ONE-SHOT</div>
+		<div class="menu">
+			<template v-if="!login">
+				<router-link :to="{ name: 'login' }">로그인</router-link>
+				<router-link :to="{ name: 'signup' }">회원가입</router-link>
+			</template>
+			<template v-else>
+				<router-link :to="{ name: 'order-brand' }">주문 하기</router-link>
+			</template>
+		</div>
+	</div>
 </template>
 <!-- --------------------------------------------------------------- -->
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const islogin = () => {
+	if (localStorage.getItem("token")) {
+		return true;
+	}
+	return false;
+};
+const login = ref(islogin());
+
+</script>
 <!-- --------------------------------------------------------------- -->
-<style scoped></style>
+<style scoped>
+.background {
+	background-image: url("/src/assets/coffee.jpg");
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	height: 100vh;
+	width: 100vw;
+	position: relative;
+	overflow: hidden;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
+
+.title {
+	font-size: 11vw;
+	font-weight: 900;
+	color: white;
+}
+
+a {
+	margin: 0 1rem;
+	font-size: 4vw;
+	text-decoration: none;
+	color: white;
+}
+
+a:hover {
+	color: black;
+}
+
+@media screen and (min-width: 770px) {
+	a {
+		font-size: 2.5vw;
+	}
+}
+</style>
