f58975f39756da7495e353eeef6227e813d84300
nijesmik <bkybb@duck.com>
2023-11-23 16:22:11 +0900

    Fix(FE): 회원가입 화면을 bootstrap에서 vuetify로 변경 (#30)

--- OneShot_Vue/src/views/SignupView.vue
+++ OneShot_Vue/src/views/SignupView.vue
@@ -1,31 +1,83 @@
 <template>
-  <form class="row g-3">
-  <h2>Sign up</h2>
-  <br/>
-  <div class="mb-3">
-    <label for="email" class="form-label">Email address</label>
-    <input type="email" class="form-control form-control-lg" id="email" aria-describedby="emailHelp" v-model="email">
-  </div>
-  <div class="mb-3 col-md-6">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" class="form-control form-control-lg" id="password" v-model="password">
-  </div>
-  <div class="mb-3 col-md-6">
-    <label for="password2" class="form-label">Password 확인</label>
-    <input type="password" class="form-control form-control-lg" id="password2" v-model="password2">
-  </div>
-  <div class="mb-3">
-    <label for="nickname" class="form-label">Nickname</label>
-    <input type="text" class="form-control form-control-lg" id="nickname" v-model="nickname">
-  </div>
-  <button type="submit" class="btn btn-primary btn-lg" @click.prevent="signup">Submit</button>
-</form>
+	<v-container>
+		<div class="title"></div>
+		<v-card
+			class="mx-auto pa-12 pb-8"
+			elevation="8"
+			max-width="448"
+			rounded="lg"
+		>
+			<div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
+			<v-text-field
+				density="compact"
+				placeholder="Email address"
+				prepend-inner-icon="mdi-email-outline"
+				variant="outlined"
+				v-model="email"
+			></v-text-field>
 
+			<div
+				class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+			>
+				Password
+			</div>
+			<v-text-field
+				:append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+				:type="visible ? 'text' : 'password'"
+				density="compact"
+				placeholder="Enter your password"
+				prepend-inner-icon="mdi-lock-outline"
+				variant="outlined"
+				@click:append-inner="visible = !visible"
+				v-model="password"
+			></v-text-field>
+
+			<!-- password2 -->
+			<div
+				class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+			>
+				Password 확인
+			</div>
+			<v-text-field
+				:append-inner-icon="visible2 ? 'mdi-eye-off' : 'mdi-eye'"
+				:type="visible2 ? 'text' : 'password'"
+				density="compact"
+				placeholder="Enter your password"
+				prepend-inner-icon="mdi-lock-outline"
+				variant="outlined"
+				@click:append-inner="visible2 = !visible2"
+				v-model="password2"
+			></v-text-field>
+
+			<div
+				class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+			>
+				Nickname
+			</div>
+			<v-text-field
+				density="compact"
+				placeholder="Enter your nickname"
+				variant="outlined"
+				v-model="nickname"
+			></v-text-field>
+
+			<v-btn
+				block
+				class="mb-8 mt-8"
+				color="blue"
+				size="large"
+				variant="tonal"
+				@click.prevent="signup"
+			>
+				Submit
+			</v-btn>
+		</v-card>
+	</v-container>
 </template>
 <!-- --------------------------------------------------------------- -->
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
 import { useUrlStore } from "@/stores/url";
@@ -37,35 +89,40 @@ const password = ref("");
 const password2 = ref("");
 const nickname = ref("");
 
+const visible = ref(false);
+const visible2 = ref(false);
+
 const signup = () => {
-  if (password.value !== password2.value) {
-    alert("비밀번호가 일치하지 않습니다.");
-    return;
-  }
-  axios.post(URL.API.SIGNUP, {
-    email: email.value,
-    password: password.value,
-    nickname: nickname.value,
-  }).then((res)=>{
-    if (res.data === 1) {
-      alert("회원가입 성공");
-      router.push({name: "login"});
-    }
-  }).catch((err) => {
-    alert("회원가입 실패");
-    console.log(err);
-  });
-
-}
-
-
+	if (password.value !== password2.value) {
+		alert("비밀번호가 일치하지 않습니다.");
+		return;
+	}
+	axios
+		.post(URL.API.SIGNUP, {
+			email: email.value,
+			password: password.value,
+			nickname: nickname.value,
+		})
+		.then((res) => {
+			if (res.data === 1) {
+				alert("회원가입 성공");
+				router.push({ name: "login" });
+			}
+		})
+		.catch((err) => {
+			alert("회원가입 실패");
+			console.log(err);
+		});
+};
 </script>
 <!-- --------------------------------------------------------------- -->
 <style scoped>
-
 form {
-  width: 90%;
-  max-width: 900px;
+	width: 90%;
+	max-width: 900px;
 }
 
+.title {
+	margin-top: 10%;
+}
 </style>
