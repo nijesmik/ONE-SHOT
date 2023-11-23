cf23fc862b298d40ed9860cb12f766be6f367120
nijesmik <bkybb@duck.com>
2023-11-23 16:39:40 +0900

    Fix(FE): 창 크기에 따라 한 줄에 보여지는 메뉴 개수가 달라지도록 수정 (#32)

--- OneShot_Vue/src/components/Menu.vue
+++ OneShot_Vue/src/components/Menu.vue
@@ -7,7 +7,7 @@
 		</v-card>
 
 		<v-dialog v-model="dialog" activator="parent" width="auto">
-			<v-card width="450">
+			<v-card :width="width">
 				<MenuDetailModal :menu="menu" />
 				<v-card-actions>
 					<v-btn color="red" block @click="dialog = false">Close</v-btn>
@@ -21,6 +21,14 @@
 import { useMenuStore } from "@/stores/menu";
 import { defineProps, ref, watch } from "vue";
 import MenuDetailModal from "./MenuDetailModal.vue";
+
+const calcWidth = () => {
+	if (window.innerWidth < 500) {
+		return window.innerWidth * 0.9;
+	}
+	return 450;
+};
+const width = ref(calcWidth());
 
 const menuStore = useMenuStore();
 
