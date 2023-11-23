0c19221c4ff0e7f0e6d208881540888bb285c17e
nijesmik <bkybb@duck.com>
2023-11-23 16:51:00 +0900

    Style(FE): 결과 화면 스타일 수정 (#47)
    - 가운데 정렬
    - title 추가

--- OneShot_Vue/src/views/OrderResultView.vue
+++ OneShot_Vue/src/views/OrderResultView.vue
@@ -1,8 +1,11 @@
 <template>
 	<v-container>
-		<v-list lines="two" v-for="orderDetail in orderDetails">
-			<Result :order-detail="orderDetail" />
-		</v-list>
+		<v-card max-width="600" class="mx-auto p-3 mt-3">
+			<span class="title">주문 내역</span>
+			<v-list lines="two" v-for="orderDetail in orderDetails">
+				<Result :order-detail="orderDetail" />
+			</v-list>
+		</v-card>
 	</v-container>
 </template>
 <!-- --------------------------------------------------------------- -->
@@ -30,4 +33,14 @@ axios
 	});
 </script>
 <!-- --------------------------------------------------------------- -->
-<style scoped></style>
+<style scoped>
+.list {
+	width: auto;
+	margin: 0 auto;
+}
+
+.title {
+	font-size: 2.5rem;
+	font-weight: 500;
+}
+</style>
