5f533e7d485cee5ab98feb7e413440501045fba8
nijesmik <bkybb@duck.com>
2023-11-23 16:15:46 +0900

    Feat(FE): font 추가

--- OneShot_Vue/src/App.vue
+++ OneShot_Vue/src/App.vue
@@ -4,4 +4,16 @@
 <!-- --------------------------------------------------------------- -->
 <script setup></script>
 <!-- --------------------------------------------------------------- -->
-<style></style>
+<style>
+@font-face {
+	font-family: "NanumSquareNeo";
+	src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/NanumSquareNeo-Variable.woff2")
+		format("woff2");
+	font-weight: normal;
+	font-style: normal;
+}
+
+* {
+	font-family: "NanumSquareNeo";
+}
+</style>
