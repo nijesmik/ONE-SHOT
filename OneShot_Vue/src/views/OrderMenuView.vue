95ab5172131cc3e8b6dcaa3abec6cd33314e28da
nijesmik <bkybb@duck.com>
2023-11-23 16:49:42 +0900

    Fix(FE): 메뉴 선택 화면 수정 (#32)
    - 잘못된 경로 입력시 경고창 수정
    - 창 크기에 따라 보여지는 메뉴 개수가 달라지도록 수정

--- OneShot_Vue/src/views/OrderMenuView.vue
+++ OneShot_Vue/src/views/OrderMenuView.vue
@@ -1,19 +1,34 @@
 <template>
-	<v-container>
-		<ShareUrl :url="shareUrl" />
+	<v-container v-if="!errorState">
+		<ShareUrl />
 		<MenuTypeBtn :types="menuTypes" />
 		<v-row>
 			<template v-for="menu in computedMenus">
-				<v-col cols="2">
+				<v-col :cols="cols">
 					<Menu :menu="menu" />
 				</v-col>
 			</template>
 		</v-row>
 	</v-container>
+
+	<!-- alert -->
+	<div>
+		<v-alert
+			type="error"
+			v-model="errorState"
+			variant="tonal"
+			closable
+			close-label="Close Alert"
+			title="잘못된 URL입니다."
+			class="mt-10 alert"
+		>
+			우측 닫기 버튼을 누르면 홈화면으로 이동합니다.
+		</v-alert>
+	</div>
 </template>
 <!-- --------------------------------------------------------------- -->
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useUrlStore } from "@/stores/url";
 import { useMenuStore } from "@/stores/menu";
@@ -26,41 +41,77 @@ const URL = useUrlStore();
 const menuStore = useMenuStore();
 const route = useRoute();
 const router = useRouter();
-const orderCode = route.params.orderCode;
+menuStore.orderCode = route.params.orderCode;
 
-const shareUrl = ref(`${URL.DOMAIN}/order/${orderCode}`);
+const calcCols = () => {
+	if (window.innerWidth < 600) {
+		return 4;
+	}
+	return 2;
+};
+const cols = ref(calcCols());
+
 const order = ref({});
 const orderDetails = ref([]);
 const menuTypes = ref([]);
 const menus = ref([]);
+const errorState = ref(false);
+
+watch(errorState, (state) => {
+	if (!state) {
+		router.push({ name: "home" });
+	}
+});
 
 const computedMenus = computed(() => {
+	if (menuStore.type === "ALL") {
+		return menus.value;
+	}
 	return menus.value.filter((menu) => menu.type === menuStore.type);
 });
 
+const test = (details, id) => {
+	const name = localStorage.getItem(`oneshot-order-${id}`);
+	if (name === null) {
+		return false;
+	}
+	return details.filter((detail) => detail.name === name).length > 0;
+};
+
 axios
-	.get(`${URL.API.ORDER}?orderCode=${orderCode}`)
+	.get(`${URL.API.ORDER}?orderCode=${menuStore.orderCode}`)
 	.then((res) => {
 		order.value = res.data.order;
 		menuStore.orderId = order.value.orderId;
 		orderDetails.value = res.data.orderDetail;
-		axios.get(`${URL.API.MENU}?brandId=${order.value.brandId}`).then((res) => {
-			menuTypes.value = res.data.menuType;
-			menus.value = res.data.menuList;
-			menuStore.type = menuTypes.value[0];
-		});
+		if (test(orderDetails.value, menuStore.orderId)) {
+			router.push({ name: "order-result", params: { orderCode: menuStore.orderCode } });
+		} else {
+			axios
+				.get(`${URL.API.MENU}?brandId=${order.value.brandId}`)
+				.then((res) => {
+					menuTypes.value = ["ALL", ...res.data.menuType];
+					menus.value = res.data.menuList;
+					menuStore.type = menuTypes.value[0];
+				});
+		}
 	})
 	.catch((err) => {
 		if (err.response.status === 500) {
-			alert("잘못된 URL입니다.");
-			router.push({ name: "home" });
+			errorState.value = true;
 		}
 	});
 </script>
 <!-- --------------------------------------------------------------- -->
 <style scoped>
-.type {
-	display: flex;
-	flex-wrap: wrap;
+.alert {
+	margin: 0 auto;
+	width: 50%;
+}
+
+@media screen and (max-width: 900px) {
+	.alert {
+		width: 80%;
+	}
 }
 </style>
