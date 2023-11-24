e4feda2c1cfac5d2a5f73436925fab485793efd1
nijesmik <bkybb@duck.com>
2023-11-24 09:36:50 +0900

    Feat(FE): 메뉴 상세 선택 창에 가격 표시 (#62)

---
+++ OneShot_Vue/src/components/MenuDetailModalPrice.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-card-text>
+		<div class="d-flex justify-space-between align-center">
+			<span class="subheading">주문 금액</span>
+			<span class="price">{{ price }}원</span>
+		</div>
+	</v-card-text>
+</template>
+<!-- --------------------------------------------------------------- -->
+<script setup>
+import { useMenuStore } from "@/stores/menu";
+import { computed } from "vue";
+
+const menuStore = useMenuStore();
+
+const price = computed(() => {
+	return menuStore.menuDetail.price * menuStore.amount;
+});
+</script>
+<!-- --------------------------------------------------------------- -->
+<style scoped>
+.price {
+	font-size: 1.3rem;
+}
+</style>
